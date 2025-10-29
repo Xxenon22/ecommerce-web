c9f8b74333c056d71ec92f3ab408cdb4089ec111
veriarajatunggal <veriarajatunggal@gmail.com>
2025-10-29 11:38:35 +0700

    Added Migration All Database

---
+++ database/migrations/2025_10_29_033959_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_user')->constrained('users')->onDelete('cascade');
+            $table->foreignId('id_product')->constrained('products')->onDelete('cascade');
+            $table->integer('qty');
+            $table->foreignId('id_expedition')->constrained('expeditions')->onDelete('cascade');
+            $table->integer('expedition_price')->default(0);
+            $table->integer('total_price');
+            $table->string('status')->default('pending');
+            $table->foreignId('id_payment_method')->constrained('payment_methods')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
